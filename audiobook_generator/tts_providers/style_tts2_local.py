180f2cf8e881868ed962cb8366f3353e80838ec5
AndrzejMiskow <70725641+AndrzejMiskow@users.noreply.github.com>
2024-07-07 22:40:19 +0100

    Support for Long inference on LJModel

--- audiobook_generator/tts_providers/style_tts2_local.py
+++ audiobook_generator/tts_providers/style_tts2_local.py
@@ -10,7 +10,7 @@ from audiobook_generator.core.utils import split_text, set_audio_tags
 from audiobook_generator.tts_providers.base_tts_provider import BaseTTSProvider
 
 
-import style_tts2.styletts2importable 
+import style_tts2.styletts2importable as styletts2importable
 import style_tts2.ljspeechimportable as ljspeechimportable
 import torch
 import os
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 def get_supported_models():
     #! For now adding support for the basic LJ Speech model
-    return ["LJSpeech", "Voice_Cloning" , "Multi-Voice"]
+    return ["LJSpeech", "LJSpeech-Long","Voice_Cloning" , "Multi-Voice"]
 
 
 def get_supported_voices():
@@ -54,33 +54,57 @@ class StyleTTS2Local(BaseTTSProvider):
         return super().__str__()
 
     def text_to_speech(self, text: str, output_file: str, audio_tags: AudioTags):
-        max_chars = 50000  #?  should be less than <50k characters (Not sure why)
-
-        texts = txtsplit(text)
-
-        noise = torch.randn(1,1,256).to('cuda' if torch.cuda.is_available() else 'cpu')
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         audio_segments = []
 
-        for i, chunk in enumerate(texts, 1):
-            logger.debug(
-                f"Processing chunk {i} of {len(texts)}, length={len(chunk)}, text=[{chunk}]"
-            )
-            logger.info(
-                f"Processing chapter-{audio_tags.idx} <{audio_tags.title}>, chunk {i} of {len(texts)}"
-            )
+        if self.config.model_name == "LJSpeech-Long":
+            texts = txtsplit(text)
+            s_prev = None # previus text adding to keep constantcy to the model
+            for i, chunk in enumerate(texts , 1):
+                logger.debug(
+                    f"Processing chunk {i} of {len(texts)}, length={len(chunk)}, text=[{chunk}]"
+                )
 
-            logger.debug(f"Text: [{chunk}], length={len(chunk)}")
+                logger.info(
+                    f"Processing chapter-{audio_tags.idx} <{audio_tags.title}>, chunk {i} of {len(texts)}"
+                )
 
-            audio_segments.append(ljspeechimportable.inference(chunk, noise, 
-                                                               diffusion_steps=self.config.diffusion_steps, 
-                                                               embedding_scale=1))
-            logger.debug(f"Generated audio segment {i}")
+                logger.debug(f"Text: [{chunk}], length={len(chunk)}")
+
+
+                if text.strip() == "": continue
+                text += '.' # add it back
+                noise = torch.randn(1,1,256).to(device)
+                audio , s_prev = ljspeechimportable.LFinferenceLong(chunk, s_prev, noise, 
+                                                     diffusion_steps=self.config.diffusion_steps, 
+                                                     embedding_scale=1)
+                audio_segments.append(audio)
+                logger.debug(f"Generated audio segment {i}")
+
+        else:
+            texts = txtsplit(text)
+            for i, chunk in enumerate(texts, 1):
+                noise = torch.randn(1,1,256).to(device)
+
+                logger.debug(
+                    f"Processing chunk {i} of {len(texts)}, length={len(chunk)}, text=[{chunk}]"
+                )
+                logger.info(
+                    f"Processing chapter-{audio_tags.idx} <{audio_tags.title}>, chunk {i} of {len(texts)}"
+                )
+
+                logger.debug(f"Text: [{chunk}], length={len(chunk)}")
+
+                audio_segments.append(ljspeechimportable.inference(chunk, noise, 
+                                                                diffusion_steps=self.config.diffusion_steps, 
+                                                                embedding_scale=1))
+                logger.debug(f"Generated audio segment {i}")
 
         # Combine all audio segments
         combined_audio = np.concatenate(audio_segments)
 
-        sample_rate = 24000  # Adjust this to match your model's output sample rate
+        sample_rate = 24000  
         wavfile.write(output_file, sample_rate, combined_audio)
         
         logger.info(f"Saved audio to {output_file}")
